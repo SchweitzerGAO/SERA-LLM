822a4fcbd6725f2e2c4e79cbc8db5d8d3b899fad
Charles Gao <1030587461@qq.com>
2024-03-06 23:36:16 +0800

    Day 1: Description-as-Document with Prompt Engineering with ChatGPT WebUI

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # SEALM
-Search Engine Augmented LM
+*Search Engine Augmented Language Model*
+
+## Day 1: Description-as-Document with Prompt Engineering with ChatGPT WebUI
+
+1. Completed description-as-document retrieval from google
+See [here](./sealm/search.py)
+
+2. Tested prompts for ChatGPT for prompt deep-processing and prompt-RAG
+For prompt deep-processing, a tested system input for ChatGPT is:
+```python
+"""
+            你是一个诚实的问答助手，如果你认为你能直接回答以下问题，则按以下格式输出：“答案: 该问题的答案”，否则，请针对以下问题生成{k}个更加深入的问题并直接输出。
+            注意：
+            1. 总是生成易于进行搜索引擎检索的问题。例如，不要生成类似“您……”或“你……”的问题，
+            2. 请必须以JSON列表的形式返回问题
+            问题：{question}
+        """
+```
+The model is more prone to generate questions than answer it directly although it knows the answer
+For in-prompt RAG(which means references are in the prompt), a tested prompt for ChatGPT is:
+
+```python
+"""
+            你是一个诚实的问答助手，以下是用户的问题以及可能有用的文档，文档以 “序号：内容” 的格式给出：
+            问题：{question}
+            文档：
+            ...
+            {docs}
+            ...
+            请逐步深入思考问题与文档的关系，并在思考的过程中注意：
+            1. 如果你不能直接回答这个问题，则参考文档尝试回答
+            2. 如果你参考文档回答了这个问题，请把参考文档的序号以Markdown上标的形式标注在相关回答之后
+            3. 如果你认为参考文档无法帮助你回答这个问题，请回答“我还不了解这个问题的答案”
+            4. 不要输出与问题不相关的文字
+            尝试回答这个问题
+        """
+```
+
+However, this do not always work properly. ChatGPT tend to drop into hallucination than admit that it doesn't know this when the documents is irrelavent. Among these malfunctions there is [a surprising one](https://chat.openai.com/share/9c26faf9-4ad7-4430-ae03-2ca15833c628) which shows in multi-turn dialogue ChatGPT **can admit its incapability in answering the question only after the 1st turn of dialogue**
+
+## Day 2: Description-as-Document with Prompt Engineering with ChatGPT API
---
+++ sealm/LLM/utils/search.py
@@ -0,0 +1,58 @@
+from googlesearch import search
+from typing import List, Union
+
+
+def desc_as_doc_search(query: Union[List, str], 
+                  num_results_single=10, # number of results for a single query
+                  num_results_multi=2, # number of results for multiple queries
+                  lang='zh-CN', 
+                  advanced=True):
+    """
+    The 'description' field in the search result are deemed directly as the reference docs
+    """
+    documents = []
+    if isinstance(query, List):
+        for q in query:
+            results = search(q, 
+                             num_results=num_results_multi,
+                             lang=lang, 
+                             advanced=advanced)
+            for result in results:
+                doc = result.description.replace("\xa0...","").replace("...","")
+                if '—' in doc:
+                    doc = doc.split('—')[1]
+                    documents.append(doc)
+    else:
+        results = search(query, 
+                         num_results=num_results_single, 
+                         lang=lang, 
+                         advanced=advanced)
+        for result in results:
+            doc = result.description.replace("\xa0...","").replace("...","")
+            if '—' in doc:
+                doc = doc.split('—')[1]
+            documents.append(doc)
+    return documents
+
+def page_as_doc_search(
+                  query: Union[List, str], 
+                  num_results_single=10, # number of results for a single query
+                  num_results_multi=2, # number of results for multiple queries
+                  lang='zh-CN', 
+                  advanced=True):
+    """
+    Store webpages in the result in a vector database using LangChain(or llama index), which is deemed as reference docs.
+    And the LLM will retrieve again in the database to get a answer more accurate
+    """
+    pass
+
+
+"""
+Test code below
+""" 
+# docs = desc_as_doc_search("唐宋八大家都是谁")
+# # docs = desc_as_doc_search(["妙玉在《红楼梦》中的人物形象如何反映了中国传统文化中对女性角色的理解与塑造？",
+# #                       "妙玉的性格特点和行为举止如何在小说中得到体现？这些特点如何与她的家庭背景和成长环境相关联？",
+# #                       "《红楼梦》中，妙玉与其他女性角色的关系是怎样的？她与宝玉、黛玉等其他主要人物之间的互动如何揭示了她的个性与情感状态？"])
+
+# print(docs)
